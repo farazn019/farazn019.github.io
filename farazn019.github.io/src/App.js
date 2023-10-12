3bf13065287262cdb5245c525c4502063a8d54ff
Faraz Naseem <naseemf@uwindsor.ca>
2023-10-12 02:50:26 -0400

    Added my resume.

--- farazn019.github.io/src/App.js
+++ farazn019.github.io/src/App.js
@@ -4,7 +4,32 @@ import { AboutMe } from './AboutMe';
 import './App.css';
 import { WorkExperience } from './WorkExperience';
 
+
+import Faraz_Naseem_Resume from './pdf_files/Faraz_Naseem_Resume.pdf';
+
+import { Document, Page, pdfjs} from 'react-pdf';
+import { useState } from 'react';
+
+/*pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.js',
+  import.meta.url,
+).toString();*/
+
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 function App() {
+
+
+  /*const [numPages, setNumPages] = useState(1);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess (numPages) {
+    setNumPages(numPages);
+  }*/
+
+  const resume = './Faraz_Naseem_Resume.pdf';
+
+
   return (
     <div className="app">
       
@@ -20,17 +45,33 @@ function App() {
         <WorkExperience/>
       </div>
 
-      <div className="resume" id="resume">
+      <div className="embed-responsive" id="resume" style = {{height: '100vh'}}>
 
+        {/*<PDFViewer>
+          <PDFDocument></PDFDocument>
+        </PDFViewer>*/}
+
+        {/*<Document options={{workerSrc: './pdf_files/Faraz_Naseem_Resume.pdf'}}>
+          <Page></Page>
+        </Document>*/}
+        <div style={{width: 600}} className="resume-document">
+          <Document file={resume}>
+            <Page pageNumber={1} renderTextLayer={false} renderAnnotationLayer={false} customTextRenderer={false}></Page> 
+            <Page pageNumber={2} renderTextLayer={false} renderAnnotationLayer={false} customTextRenderer={false}></Page> 
+          </Document> 
+
+
+
+        </div>
       </div>
 
-      <div className="side-projects" id="side-projects">
+      {/*<div className="side-projects" id="side-projects">
 
       </div>
 
       <div className="contact-me" id="contact-me">
 
-      </div>
+      </div>*/}
     </div>
   );
 }
